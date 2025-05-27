ea025738c210ba675a37beb7cfb1c77248358dcd
zaenal-abidin-syah <aszaenal618@gmail.com>
2025-05-28 06:52:18 +0700

    build ulang

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const kegiatan = require("./routes/kegiatan");
-const laporan = require("./routes/laporan");
+// const kegiatan = require("./routes/kegiatan");
+// const laporan = require("./routes/laporan");
 
 app.use(express.json());
 
-app.use("/kegiatan", kegiatan);
-app.use("/laporan", laporan);
-
+// app.use("/kegiatan", kegiatan);
+// app.use("/laporan", laporan);
+app.get("/", (req, res) => {
+  res.send("Welcome to the Kegiatan API");
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listenning at http://localhost:${port}`);
 });
