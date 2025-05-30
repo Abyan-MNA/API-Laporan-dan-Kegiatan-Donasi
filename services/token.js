43f6927ed0801d702e29c88ff56a3462ea1dc9eb
Abyan-MNA <abyanu@163.com>
2025-05-31 04:55:30 +0700

    Auth to donation-api-auth.vercel.app

--- services/token.js
+++ services/token.js
@@ -1,18 +1,26 @@
-const jwt = require("jsonwebtoken");
+const axios = require('axios');
 const dotenv = require("dotenv");
 dotenv.config();
-const authenticateToken = (req, res, next) => {
+const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ message: "Membutuhkan JWT access token" });
+    return res.status(401).json({ message: "Akses token dibutuhkan. Silahkan login dengan metode POST https://donation-api-auth.vercel.app/auth/login untuk mendapatkan token" });
   }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      return res.status(401).json({ message: "Verifikasi token gagal" });
+  try {
+    const response = await axios.post('https://donation-api-auth.vercel.app/auth/verify-token', {token});
+
+    if (response.data.valid) {
+      req.user = response.data.user;
+      next();
+    } else {
+      return res.status(403).json({ message: "Token tidak valid/sah" });
     }
-    req.user = user; // Attach user info to request
-  });
+  }
+  catch (error) {
+    console.error("Galat/error memverifikasi token:", err.message);
+    return res.status(500).json({ message: "Gagal memverifikasi token" })
+  }
   next();
 };
 
